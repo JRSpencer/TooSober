e23528dd53698570c2fb2e490f73e8a76e518837
James Spencer <JRSpencer00@gmail.com>
2016-07-18 20:32:29 -0400

    New Content: Add and Remove Drinks via arrayList Calculate in menu bar BAC decreases over time Time selector finished

--- app/src/main/java/com/jrspencer00/imtoosoberforthissht/MainActivity.java
+++ app/src/main/java/com/jrspencer00/imtoosoberforthissht/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,7 +16,9 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView largeDrinksDisplay;
     private TextView timeUntilDeparture;
     private TextView currentBAC;
-    private TextView desiredBAC;
+    private TextView BACGoal;
 
     private EditText userInputFeet;
     private EditText userInputInches;
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button calculate;
     private Button addDrink;
+    private Button subtractDrink;
 
     private RadioButton genderMale;
     private RadioButton genderFemale;
@@ -52,7 +56,11 @@ public class MainActivity extends AppCompatActivity {
             0.25,
             0.3};
 
-    double alcoholInGrams;
+    List<Integer> drinkSizeArray = new ArrayList<Integer>();
+    List<Double> drinkPercentArray = new ArrayList<Double>();
+
+    double alcoholInGrams = 0;
+    int minutesUntilDeparture;
 
 
     @Override
@@ -80,13 +88,14 @@ public class MainActivity extends AppCompatActivity {
 
         calculate = (Button) findViewById(R.id.calculate);
         addDrink = (Button) findViewById(R.id.addDrink);
+        subtractDrink = (Button) findViewById(R.id.substractDrink);
 
         drunkLevel = (Spinner) findViewById(R.id.drunkLevel);
 
         largeDrinksDisplay = (TextView) findViewById(R.id.drinksDisplay);
         timeUntilDeparture = (TextView) findViewById(R.id.timeRemaining);
         currentBAC = (TextView) findViewById(R.id.currentBAC);
-        desiredBAC = (TextView) findViewById(R.id.desiredBAC);
+        BACGoal = (TextView) findViewById(R.id.desiredBAC);
 
         genderMale = (RadioButton) findViewById(R.id.genderMale);
         genderFemale = (RadioButton) findViewById(R.id.genderFemale);
@@ -94,88 +103,51 @@ public class MainActivity extends AppCompatActivity {
         timePM = (RadioButton) findViewById(R.id.pmTime);
 
 
-               calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (userInputFeet.getText().length() > 0 && userInputInches.getText().length() > 0 &&
-                        userInputWeight.getText().length() > 0 && (genderFemale.isChecked() || genderMale.isChecked()) &&
-                        selectedHour.getText().length() > 0 && selectedMinute.getText().length() > 0 &&
-                        (timeAM.isChecked() || timePM.isChecked())) {
-                    double genderConstant;
-                    long drunkLevelValue = drunkLevel.getSelectedItemId();
-                    double desiredBAC = BAC[(int) drunkLevelValue];
-//                    Toast.makeText(MainActivity.this, "Desired BAC is " + desiredBAC, Toast.LENGTH_LONG).show();
-                    int totalHeightInches = (12 * Integer.parseInt(userInputFeet.getText().toString())) + Integer.parseInt(userInputInches.getText().toString());
-//                    outputHeight.setText(Integer.toString(totalHeightInches));
-                    int userWeight = Integer.parseInt(userInputWeight.getText().toString());
-
-//                    Toast.makeText(MainActivity.this, "weight is " + userWeight, Toast.LENGTH_LONG).show();
-                    double weightInGrams = userWeight * 453.592;
-
-                    if (genderMale.isChecked()) {
-                        genderConstant = 0.68;
-                    } else if (genderFemale.isChecked()) {
-                        genderConstant = 0.55;
-                    } else{
-                        Toast.makeText(MainActivity.this, "Please select a gender", Toast.LENGTH_LONG).show();
-                        genderConstant = 0;
-                    }
-
-                    int drinkSizeValue = Integer.parseInt(drinkSize.getText().toString());
-                    double alcoholContent = Double.parseDouble(percentageAlcohol.getText().toString());
-                    double drinksNeeded = (desiredBAC / 100) * weightInGrams * genderConstant;
-                    alcoholInGrams = calculateAlcoholInGrams(drinkSizeValue, alcoholContent);   //drinkSizeValue * (alcoholContent / 100) * 0.789;
-                    Toast.makeText(MainActivity.this, "Alcohol in Gram is " + alcoholInGrams, Toast.LENGTH_LONG).show();
-//                    drinksNeeded = Math.round(drinksNeeded * 100 / alcoholInGrams);
-//                    drinksNeeded /= 100;
-                    drinksNeeded = round(drinksNeeded / alcoholInGrams);
-//                    displayDrinksNeeded.setText(Double.toString(drinksNeeded));
-                    largeDrinksDisplay.setText(Double.toString(drinksNeeded) + " drinks needed to reach desired level of drunkenness");
-                    int hour = Integer.parseInt(selectedHour.getText().toString());
-                    if(Integer.parseInt(selectedHour.getText().toString()) > 0 &&
-                            Integer.parseInt(selectedHour.getText().toString()) < 13 ){
-                        if(Integer.parseInt(selectedMinute.getText().toString()) >= 0 &&
-                                Integer.parseInt(selectedMinute.getText().toString()) < 60 ){
-                            Calendar cal = Calendar.getInstance();
-                            if (timePM.isChecked() && hour < 12) {
-                                hour += 12;
-                            }
-                            int hourDifference;
-                            if (hour < cal.get(Calendar.HOUR_OF_DAY)) {
-                                hourDifference = hour - cal.get(Calendar.HOUR_OF_DAY) + 24;
-                            }else{
-                                hourDifference = hour  - cal.get(Calendar.HOUR_OF_DAY);
-                            }
-
-                            int minuteDifference = Integer.parseInt(selectedMinute.getText().toString()) - cal.get(Calendar.MINUTE);
-                            int minutesUntilDeparture = hourDifference * 60 + minuteDifference;
-                            timeUntilDeparture.setText(Integer.toString(minutesUntilDeparture) + " Minutes until you want to leave.");
-                            double minutesPerDrink = round(minutesUntilDeparture / drinksNeeded);
-                            timeUntilDeparture.setText("You need to have a drink every " + Double.toString(minutesPerDrink) + " minutes");
-                        }else{
-                            Toast.makeText(MainActivity.this, "INVALID MINUTES", Toast.LENGTH_LONG).show();
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this, "INVALID HOUR", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Please Fill in the Content", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
+//        calculate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//        });
 
         addDrink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (drinkSize.getText().length() > 0 && percentageAlcohol.getText().length() > 0) {
-                    int drinkSizeValue = Integer.parseInt(drinkSize.getText().toString());
-                    double alcoholContent = Double.parseDouble(percentageAlcohol.getText().toString());
-                    alcoholInGrams = calculateAlcoholInGrams(drinkSizeValue, alcoholContent);
+                    drinkSizeArray.add(Integer.parseInt(drinkSize.getText().toString()));
+                    drinkPercentArray.add(Double.parseDouble(percentageAlcohol.getText().toString()));
+                    //int drinkSizeValue = Integer.parseInt(drinkSize.getText().toString());
+                    //double alcoholContent = Double.parseDouble(percentageAlcohol.getText().toString());
+                    //alcoholInGrams = calculateAlcoholInGrams(drinkSizeValue, alcoholContent);
+
+//                    alcoholInGrams = 0;
+//                    for (int i = 0; i < drinkPercentArray.size(); i++) {
+                    alcoholInGrams = alcoholInGrams + calculateAlcoholInGrams(drinkSizeArray.get(drinkSizeArray.size() - 1),
+                            drinkPercentArray.get(drinkPercentArray.size() - 1));
                     double drinksPerDrink = round(alcoholInGrams / 14);
                     currentBAC.setText(Double.toString(drinksPerDrink));
+//                    }
+
                 } else {
                     Toast.makeText(MainActivity.this, "Please fill out drink size", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        subtractDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drinkSize.getText().length() > 0 && percentageAlcohol.getText().length() > 0 && drinkSizeArray.size() > 0) {
+//                    drinkSizeArray.add(Integer.parseInt(drinkSize.getText().toString()));
+//                    drinkPercentArray.add(Double.parseDouble(percentageAlcohol.getText().toString()));
+                    alcoholInGrams = alcoholInGrams - calculateAlcoholInGrams(drinkSizeArray.get(drinkSizeArray.size() - 1),
+                            drinkPercentArray.get(drinkPercentArray.size() - 1));
+                    drinkSizeArray.remove(drinkSizeArray.size() - 1);
+                    drinkPercentArray.remove(drinkPercentArray.size() - 1);
+                    double drinksPerDrink = round(alcoholInGrams / 14);
+                    currentBAC.setText(Double.toString(drinksPerDrink));
+
+                } else {
+                    Toast.makeText(MainActivity.this, "No drinks to remove", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -197,10 +169,83 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            if (userInputFeet.getText().length() > 0 && userInputInches.getText().length() > 0 &&
+                    userInputWeight.getText().length() > 0 && (genderFemale.isChecked() || genderMale.isChecked()) &&
+                    selectedHour.getText().length() > 0 && selectedMinute.getText().length() > 0 &&
+                    (timeAM.isChecked() || timePM.isChecked())) {
+
+
+                int hour = Integer.parseInt(selectedHour.getText().toString());
+                if (Integer.parseInt(selectedHour.getText().toString()) > 0 &&
+                        Integer.parseInt(selectedHour.getText().toString()) < 13) {
+                    if (Integer.parseInt(selectedMinute.getText().toString()) >= 0 &&
+                            Integer.parseInt(selectedMinute.getText().toString()) < 60) {
+                        Calendar cal = Calendar.getInstance();
+                        if (timePM.isChecked() && hour < 12) {
+                            hour += 12;
+                        } else if (timeAM.isChecked() && hour == 12) {
+                            hour -= 12;
+                        }
+                        int hourDifference;
+                        if (hour < cal.get(Calendar.HOUR_OF_DAY)) {
+                            hourDifference = hour - cal.get(Calendar.HOUR_OF_DAY) + 24;
+                        } else {
+                            hourDifference = hour - cal.get(Calendar.HOUR_OF_DAY);
+                        }
+
+                        int minuteDifference = Integer.parseInt(selectedMinute.getText().toString()) - cal.get(Calendar.MINUTE);
+                        minutesUntilDeparture = hourDifference * 60 + minuteDifference;
+//                            System.out.println(minutesUntilDeparture);
+                        timeUntilDeparture.setText(Integer.toString(minutesUntilDeparture) + " Minutes until you want to leave.");
+                        //double minutesPerDrink = round(minutesUntilDeparture / drinksNeeded);
+                        //timeUntilDeparture.setText("You need to have a drink every " + Double.toString(minutesPerDrink) + " minutes");
+                    } else {
+                        Toast.makeText(MainActivity.this, "INVALID MINUTES", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "INVALID HOUR", Toast.LENGTH_LONG).show();
+                }
+
+                long drunkLevelValue = drunkLevel.getSelectedItemId();
+
+                int totalHeightInches = (12 * Integer.parseInt(userInputFeet.getText().toString())) + Integer.parseInt(userInputInches.getText().toString());
+                int userWeight = Integer.parseInt(userInputWeight.getText().toString());
+                int drinkSizeValue = Integer.parseInt(drinkSize.getText().toString());
+
+                double genderConstant;
+                double desiredBAC = BAC[(int) drunkLevelValue];
+                double alcoholContent = Double.parseDouble(percentageAlcohol.getText().toString());
+                double weightInGrams = userWeight * 453.592;
+
+                if (genderMale.isChecked()) {
+                    genderConstant = 0.68;
+                } else if (genderFemale.isChecked()) {
+                    genderConstant = 0.55;
+                } else {
+                    Toast.makeText(MainActivity.this, "Please select a gender", Toast.LENGTH_LONG).show();
+                    genderConstant = 0;
+                }
+
+//                    alcoholInGrams = calculateAlcoholInGrams(drinkSizeValue, alcoholContent);   //drinkSizeValue * (alcoholContent / 100) * 0.789;
+                Toast.makeText(MainActivity.this, "Alcohol in Gram is " + alcoholInGrams, Toast.LENGTH_LONG).show();
+
+                double hoursUntilDeparture = minutesUntilDeparture / 60;
+                double drinksNeeded = (desiredBAC / 100) * weightInGrams * genderConstant;
+//                    alcoholInGrams = 14; //Set to standard drinks, can be changed to cope with drink size but would be unreliable.
+                drinksNeeded = round((drinksNeeded / 14) + 0.015 * hoursUntilDeparture);
+//                    displayDrinksNeeded.setText(Double.toString(drinksNeeded));
+                largeDrinksDisplay.setText(Double.toString(drinksNeeded) + " drinks needed to reach desired level of drunkenness");
+                BACGoal.setText(Double.toString(drinksNeeded));
+
+
+            } else {
+                Toast.makeText(MainActivity.this, "Please Fill in the Content", Toast.LENGTH_LONG).show();
+            }
+
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
+//        return super.onOptionsItemSelected(item);
     }
 
     public static double calculateAlcoholInGrams(int drinkSize, double percentAlcohol) {
@@ -213,5 +258,13 @@ public class MainActivity extends AppCompatActivity {
         roundedValue = Math.round(Value * 100);
         roundedValue /= 100;
         return roundedValue;
+    }
+
+    public static int getHoursRemaining(int hours, int minutes) {
+        int timeRemaining = 0;
+
+        timeRemaining = hours + minutes / 60;
+
+        return timeRemaining;
     }
 }
